4fa36cc37474a7be720a6d03531321ea8e4669f8
Thomas Luu <luutoan07@gmail.com>
2022-07-04 10:44:24 +0700

    launch website in port 3000

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require('express');
+const expressLayout = require('express-layout');
+const mongoose = require('mongoose');
 const app = express();
+
+//db configuration
+const db = require('./config/key.js');
+
+
+//running
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`);
+});
+
