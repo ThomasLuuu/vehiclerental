7f6edf88cff38fefa0cd1509cceff04cb8893004
Thomas Luu <luutoan07@gmail.com>
2022-07-04 13:59:58 +0700

    connect database - mongodb

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const expressLayout = require('express-layout');
 const mongoose = require('mongoose');
 const app = express();
-
 //db configuration
-const db = require('./config/key.js');
+const db = require('./config/key.js').mongoURI;
 
-
+//connect db
+mongoose
+  .connect(
+    db,
+    {useNewUrlParser: true}
+  )
+  .then(() => {
+    console.log('Connected to MongoDB...');
+  })
+  .catch(err => console.log(err))
 //running
 const port = process.env.PORT || 3000;
 
