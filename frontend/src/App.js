f3a163d3a427b4373df843477f4be20a604d6987
PhatB <bducphat08@gmail.com>
2022-07-14 15:32:11 +0700

    added  footer and css for footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { LiveAnywhere } from './components/LiveAnywhere';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       {/* <ExploreNearby /> */}
       <LiveAnywhere />
 
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+
+const Footer = () => {
+    return (
+        <footer className='stayBot'>
+            <p>© 2022 Airbnb, Inc.   ·    Privacy   ·  Terms    ·  Sitemap    ·  Destinations     English (US)     $USD    Support & resources</p>
+        </footer>
+    )
+}
+
+export default Footer 
