09975b20bf87b171925a8a388bfef4fd2e4671ce
anhduc2708 <68212055+anhduc2708@users.noreply.github.com>
2022-07-17 09:53:31 +0700

    update component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { LiveAnywhere } from './components/LiveAnywhere';
+import ExploreNearby from './components/ExploreNearbyComponent/ExploreNearby';
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
-      {/* <ExploreNearby /> */}
+      <ExploreNearby />
       <LiveAnywhere />
 
       {/* <Footer /> */}
