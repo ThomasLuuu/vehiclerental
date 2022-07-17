202ff9943b8f2fae00c1defe1ecad060ee45e6fe
duysupernova <62131990+duysupernova@users.noreply.github.com>
2022-07-17 21:33:22 +0700

    Update header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { LiveAnywhere } from './components/LiveAnywhere/LiveAnywhere';
 import Footer from './components/Footer/Footer';
-
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       {/* <ExploreNearby /> */}
       <LiveAnywhere />
       <Footer />
