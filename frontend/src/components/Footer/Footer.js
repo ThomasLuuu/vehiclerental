a01a9d5979cc404bee3ab0676feeec3bfc5447f3
PhatB <bducphat08@gmail.com>
2022-07-20 22:39:23 +0700

    fixed minor footer mistakes

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
     return (
         <footer className='stayBot'>
             <p className='footerp1'>© 2022 Airbnb, Inc.   ·  <a href='#' >Privacy</a>   ·  <a href='#' >Terms</a>    ·  <a href='#' >Sitemap</a>    ·  <a href='#' >Destinations</a> </p> 
-            <p className='footerp2'> English (US)     $  USD     Support & resources</p>
         </footer>
     )
 }
