9ca671c71b0190f4d7e8913938665e38891b1071
Minh Nguyens <wormscott12397@gmail.com>
2022-07-20 22:05:45 +0700

    add explore nearby component to app js

--- frontend/src/components/LiveAnywhere/LiveAnywhere.js
+++ frontend/src/components/LiveAnywhere/LiveAnywhere.js
@@ -4,8 +4,10 @@ import { Img } from './Img.js';
 import { Title } from './Title.js';
 
 const rows = [];
-for (let i = 1; i <= 4; i++) {
-  rows.push(<Img loc={require('../../img/Img' + i.toString() + '.jpg')} />);
+for (let i = 1; i <= 24; i++) {
+  let remainder = i % 4;
+  if (remainder === 0) remainder = 4;
+  rows.push(<Img loc={require('../../img/Img' + remainder.toString() + '.jpg')} />);
 }
 console.log(rows);
 
