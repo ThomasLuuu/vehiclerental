86b2bae60b5ad14836dc1f929bfbe144186575cf
anhduc2708 <68212055+anhduc2708@users.noreply.github.com>
2022-07-21 14:19:00 +0700

    fixing bug in ExNearby

--- frontend/src/components/ExploreNearby/ExploreNearby.js
+++ frontend/src/components/ExploreNearby/ExploreNearby.js
@@ -1,12 +1,12 @@
 import Card from './Card';
 import Dummy_Data from './dummydata.json';
-
+import './ExploreNearby.css'
 function ExploreNearby() {
   return (
-    <div className="ExploreNearby">
+    <div id ="ExploreNearby">
+      <h2 >Explore Nearby</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid cols-4">
-        <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-
+        
         {Dummy_Data.map((data) => {
           return <Card key={data.img} img={data.img} url={data.img} location={data.location} distance={data.distance} />;
         })}
