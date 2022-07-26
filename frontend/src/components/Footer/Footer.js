74f7bd991f603bf9e4e3ad50d51dee3444172dc2
PhatB <bducphat08@gmail.com>
2022-07-26 13:10:07 +0700

    changes footer message

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer className='stayBot'>
-            <p className='footerp1'>© 2022 Airbnb, Inc.   ·  <a href='#' >Privacy</a>   ·  <a href='#' >Terms</a>    ·  <a href='#' >Sitemap</a>    ·  <a href='#' >Destinations</a> </p> 
+            <p className='footerp1'>© Six ducks ·  <a href='#' >Privacy</a>   ·  <a href='#' >Terms</a>    ·  <a href='#' >Sitemap</a>    ·  <a href='#' >Destinations</a> </p> 
         </footer>
     )
 }
