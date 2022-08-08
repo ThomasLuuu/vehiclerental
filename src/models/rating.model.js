fcf17d2d0afd04c6512a6baedc0f55a064ca983d
Minh Nguyens <wormscott12397@gmail.com>
2022-08-08 12:08:42 +0700

    update algorithm for calcualtion rating

--- src/models/rating.model.js
+++ src/models/rating.model.js
@@ -12,3 +12,8 @@ const ratingSchema = new mongoose.Schema(
 );
 const Rating = mongoose.model('Rating', ratingSchema);
 module.exports = Rating;
+
+// rating
+// Score total: 50(5) + 25(4) + 20(3) + 15(2) + 10(1) = 450
+// Response total: 50 + 25 + 20 + 15 + 10 = 120
+// 5-star score: 450 / 120 = 3.75, which rounds to 3.8 (your 5-star score)
