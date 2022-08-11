3f1c9f5a3e6656f13e3539db0e5de250ec7c3608
Thomas Luu <luutoan07@gmail.com>
2022-08-11 12:56:58 +0700

    adding loading page for website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,31 @@ import LiveAnywhere from './components/LiveAnywhere/LiveAnywhere';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import ExploreNearby from './components/ExploreNearby/ExploreNearby';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import InfiniteScroll from './components/LiveAnywhere/InfiniteScroll';
-
+import {Loading} from './components/Loading/Loading';
 function App() {
   const [query, setQuery] = useState('');
   const [pageNumber, setPage] = useState(1);
-  InfiniteScroll(query, pageNumber);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000)
+  }, [])
   return (
-    <div className="App">
+    <>
+    {loading === false ? (
+      <div className="App">
+      {/* <Loading /> */}
       <Header />
       <ExploreNearby />
       <LiveAnywhere />
       <Footer />
     </div>
+    ) : (
+      <Loading /> 
+    )}
+    </>
   );
 }
 
