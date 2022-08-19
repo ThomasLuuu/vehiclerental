f926748749c2ea4d3deeda7f466902728e7f8343
PhatB <bducphat08@gmail.com>
2022-08-19 15:21:23 +0700

    minor bug fix on DetailedPost

---
+++ frontend/src/components/DetailedPost/PostTitle.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function PostTitle(titleOFPost){
+    return(
+        <div>
+            {/* <h1>This is the post Title</h1> */}
+            <div className="lg:flex lg:items-center lg:justify-between ">
+                <div className="flex-1 min-w-0">
+                    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl  sm:truncate">Placeholder post headings</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
---
+++ frontend/src/components/DetailedPost/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {PostTitle} from "./PostTitle";
+import {InfoLine} from "./InfoLine";
+import {ShowcasePics} from "./ShowcasePics";
+
+
+
+export function FullPage(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
