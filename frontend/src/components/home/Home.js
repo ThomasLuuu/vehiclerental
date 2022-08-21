839b46f14c2bb0cd3de3d71874ca5b113e190894
ThomasLuuu <56161180+ThomasLuuu@users.noreply.github.com>
2022-08-21 13:07:29 +0700

    add location tracking

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,29 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, Component } from 'react';
 
 import bg from '../../images/bg.webp';
 
 import * as uiService from '../../services/ui';
 
-const Home = () => {
+class Home extends Component {
 
-  useEffect(() => {
-    window.onload = function () {
-      uiService.showDarkHeader();
+  getLocation = function(){
+
+    const success = (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const geoApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+      fetch(geoApi).then((response) => response.json())
+        .then(data => {
+          console.log(data.principalSubdivision)
+          console.log(data)});
     }
-  }, []);
 
-  return (
-    <div className="home">
-      <img src={bg} alt="bg" />
-      <div className="home__title">
-        <h1>Could not find the place to go?</h1>
-        <h1>Great!</h1>
-        <div className="home__button-container">
-          <button>Quick Search</button>
+    const error = (error) => {
+      console.log(error)
+    }
+
+    navigator.geolocation.getCurrentPosition(success,error);
+  };
+  componentDidMount() {
+    window.onload = function () {
+          uiService.showDarkHeader();
+        }
+    this.getLocation();
+  }
+  render(){
+    return (
+      <div className="home">
+        <img src={bg} alt="bg" />
+        <div className="home__title">
+          <h1>Could not find the place to go?</h1>
+          <h1>Great!</h1>
+          <div className="home__button-container">
+            <button>Quick Search</button>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+
 };
 
 export default Home;
