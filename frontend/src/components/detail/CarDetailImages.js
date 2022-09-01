e5d8d4a2939cb973dfd674806f6f67300e914915
Minh Nguyens <wormscott12397@gmail.com>
2022-09-01 14:58:37 +0700

    fix NotSameOriginAfterDefaultedToSameOriginByCoep when get image from firebase storage

--- frontend/src/components/detail/CarDetailImages.js
+++ frontend/src/components/detail/CarDetailImages.js
@@ -2,7 +2,7 @@ const CarDetailImages = ({ car }) => {
   return (
     <div className="car-detail__images">
       <div className="car-detail__images-left">
-        <img src={car.image} alt={car.name} />
+        <img crossorigin="anonymous" src={car.image} alt={car.name} />
       </div>
       <div className="car-detail__images-right">
         <img crossorigin="anonymous" src={car.image} alt={car.name} />
