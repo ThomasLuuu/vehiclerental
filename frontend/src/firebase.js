cf9e15f86398680efc68e0be191208fda4ba1347
Minh Nguyens <wormscott12397@gmail.com>
2022-09-02 10:14:02 +0700

    update new firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,11 +5,11 @@ import 'firebase/firestore'; // for cloud firestore
 import 'firebase/auth'; // for authentication
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBiIZP7iplZnS0Xv0AuaRHG8_L7mpjkidw',
-  authDomain: 'vehiclerental-235f8.firebaseapp.comvehiclerental-235f8.firebaseapp.com',
-  databaseURL: 'https://vehiclerental-235f8-default-rtdb.asia-southeast1.firebasedatabase.app/',
+  apiKey: 'AIzaSyAhPuJr33-yZiYLBbICRbU-fFtaOGW743k',
+  authDomain: 'vehicleweb-504bb.firebaseapp.com',
+  databaseURL: 'https://vehicleweb-504bb-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
-  storageBucket: 'vehiclerental-235f8.appspot.com',
+  storageBucket: 'vehicleweb-504bb.appspot.com',
   messagingSenderId: `${process.env.REACT_APP_FIREABSE_MESSAGING_SENDER_ID}`,
   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 };
